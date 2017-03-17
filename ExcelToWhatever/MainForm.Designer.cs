515c1b72a4a237b859de7b2d43dfc91b6c7ae6c0
amenprog <amenprog@gmail.com>
2017-03-17 16:46:01 +0200

    First commit

---
+++ ExcelToWhatever/MainForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace ExcelToWhatever
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.tbFilename = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbTemplate = new System.Windows.Forms.RichTextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(511, 10);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(92, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "&Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // tbFilename
+            // 
+            this.tbFilename.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbFilename.Location = new System.Drawing.Point(99, 12);
+            this.tbFilename.Name = "tbFilename";
+            this.tbFilename.Size = new System.Drawing.Size(406, 20);
+            this.tbFilename.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select excel file";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Template";
+            // 
+            // tbTemplate
+            // 
+            this.tbTemplate.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbTemplate.Location = new System.Drawing.Point(99, 42);
+            this.tbTemplate.Name = "tbTemplate";
+            this.tbTemplate.Size = new System.Drawing.Size(504, 320);
+            this.tbTemplate.TabIndex = 4;
+            this.tbTemplate.Text = "";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGenerate.Location = new System.Drawing.Point(511, 368);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(92, 23);
+            this.btnGenerate.TabIndex = 5;
+            this.btnGenerate.Text = "&Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Icon = Properties.Resources.excel;
+            this.ClientSize = new System.Drawing.Size(615, 405);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.tbTemplate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbFilename);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "MainForm";
+            this.Text = "Excel to whatever";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox tbFilename;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox tbTemplate;
+        private System.Windows.Forms.Button btnGenerate;
+    }
+}
+
---
+++ ExcelToWhatever/MainForm.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OfficeOpenXml;
+
+namespace ExcelToWhatever
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Generate()
+        {
+            if (!string.IsNullOrWhiteSpace(tbTemplate.Text) && 
+                !string.IsNullOrWhiteSpace(tbFilename.Text) &&
+                File.Exists(tbFilename.Text))
+            {
+
+                var filename = tbFilename.Text;
+
+                using (ExcelPackage package = new ExcelPackage(new FileInfo(filename)))
+                {
+
+                    var worksheet = package.Workbook.Worksheets.FirstOrDefault();
+
+                    var sb = new StringBuilder();
+
+                    var values = new List<object>();
+
+                    if (worksheet != null)
+                    {
+                        for (int i = 1; i <= worksheet.Dimension.End.Row; i++)
+                        {
+
+                            for (int j = 1; j <= worksheet.Dimension.End.Column; j++)
+                            {
+
+                                values.Add(worksheet.Cells[i, j].Value);
+
+                            }
+
+                            sb.Append(string.Format(tbTemplate.Text, values.ToArray()) + Environment.NewLine);
+
+                            values.Clear();
+
+                        }
+
+                        SaveAndOpen(sb);
+                    }
+                }
+
+            }
+
+        }
+
+        private static void SaveAndOpen(StringBuilder sb)
+        {
+            var outfile = Path.Combine(Application.StartupPath,
+                "whatever-" + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".txt");
+
+            File.WriteAllText(outfile, sb.ToString());
+
+            Process.Start(outfile);
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var fileDialog = new OpenFileDialog() { Filter = "Excel files (*.xlsx;*.xls) |*.xlsx;*.xls" })
+                {
+                    if (fileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        tbFilename.Text = fileDialog.FileName;
+                    }
+                }
+            }
+            catch
+            {
+                // Ignored
+            }
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            
+                Generate();
+            
+        }
+
+    }
+}
